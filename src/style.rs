0f1380dfc8ab4f12e58084b0b0419728b9130ee7
Rodrigo Valeri <rsvalerio@gmail.com>
2026-02-26 08:35:15 +0100

    refactor: introduce ansi_style! macro

--- src/style.rs
+++ src/style.rs
@@ -1,42 +1,17 @@
 //! ANSI terminal styling helpers for CLI output.
 
-pub fn cyan(s: &str) -> String {
-    format!("\x1b[36m{}\x1b[0m", s)
+macro_rules! ansi_style {
+    ($(#[$meta:meta])* $name:ident, $code:expr) => {
+        $(#[$meta])*
+        pub fn $name(s: &str) -> String {
+            format!("\x1b[{}m{}\x1b[0m", $code, s)
+        }
+    };
 }
 
-pub fn dim(s: &str) -> String {
-    format!("\x1b[2m{}\x1b[0m", s)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn cyan_wraps_with_ansi_codes() {
-        let result = cyan("test");
-        assert!(result.starts_with("\x1b[36m"));
-        assert!(result.ends_with("\x1b[0m"));
-        assert!(result.contains("test"));
-    }
-
-    #[test]
-    fn dim_wraps_with_ansi_codes() {
-        let result = dim("test");
-        assert!(result.starts_with("\x1b[2m"));
-        assert!(result.ends_with("\x1b[0m"));
-        assert!(result.contains("test"));
-    }
-
-    #[test]
-    fn cyan_empty_string() {
-        let result = cyan("");
-        assert_eq!(result, "\x1b[36m\x1b[0m");
-    }
-
-    #[test]
-    fn dim_empty_string() {
-        let result = dim("");
-        assert_eq!(result, "\x1b[2m\x1b[0m");
-    }
-}
+ansi_style!(cyan, 36);
+ansi_style!(#[allow(dead_code)] white, 37);
+ansi_style!(#[allow(dead_code)] grey, 90);
+ansi_style!(dim, 2);
+ansi_style!(green, 32);
+ansi_style!(red, 31);
